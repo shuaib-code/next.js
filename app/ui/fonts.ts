0937461538e2cec3683cc2b5553d193b7eeb0a26
Mohammad Shuaib <shuaib.cyclist@gmail.com>
2024-12-09 07:16:00 +0600

    created Dashboard with active navlink

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Fira_Code, Inter } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const firaCode = Fira_Code({
+  weight: ["500", "400", "300", "600"],
+  subsets: ["greek"],
+});
