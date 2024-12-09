f2176077074428be7bd5d6874d2d9e8c3340a10c
Mohammad Shuaib <shuaib.cyclist@gmail.com>
2024-12-09 07:23:41 +0600

    exporting lusitana font fixed

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,10 @@
-import { Fira_Code, Inter } from "next/font/google";
+import { Fira_Code, Inter, Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
+
 export const firaCode = Fira_Code({
   weight: ["500", "400", "300", "600"],
   subsets: ["greek"],
 });
+
+export const lusitana = Lusitana({ weight: ["400"], subsets: ["latin"] });
